304687f395b86f72f66b53fd5e6e7c697f25acca
OHelmyy <omar.helmy.re@gmail.com>
2025-04-04 22:41:05 +0200

    repositories

--- demo/src/main/java/com/example/asweprj/demo/repositories/UserRepository.java
+++ demo/src/main/java/com/example/asweprj/demo/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.asweprj.demo.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.asweprj.demo.models.User;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
     
 }
